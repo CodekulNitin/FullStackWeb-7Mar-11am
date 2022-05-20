c10e60cdabee1d34a3d9dde332bd21a2efbfdf81
CodekulNitin7872 <codekul.nitin@gmail.com>
2022-05-20 11:41:06 +0530

    first commit

---
+++ Array.js
@@ -0,0 +1,30 @@
+// let marks = [25,34,56,73,92,87]
+// let marks1 = [2,4,7,9]
+// marks.shift();
+// marks.unshift(23);
+// marks.splice(0,3);
+// marks.push("codekul")
+// marks.pop()
+
+//  marks = marks1 .concat = console.log(marks1)
+// marks = marks1.concat(marks)
+// console.log(marks)
+const obj = {
+    name: "john",
+    email: "aqbc@gmail.com",
+    skills: [
+      "html",
+      "css",
+      "js",
+      "reactjs",
+      "expressjs",
+      "nodejs",
+      "mongodb",
+    ],
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quis nam omnis hic totam. Expedita recusandae voluptatum, eaque veritatis sed, quae porro, est assumenda blanditiis possimus quis! Excepturi autem, nostrum tenetur eos fuga recusandae numquam, et suscipit minus perspiciatis quas unde vitae consectetur id sapiente molestiae omnis nobis eum, consequatur ullam atque pariatur soluta? Aliquam, autem?",
+  };
+  for(let key in obj){
+    console.log(obj[key])
+   }
+//   document.getElementById("demo").innerHTML = obj;
